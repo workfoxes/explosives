6f22f0b75657af2b546e266462ac44c824a579c9
Vasanth Kumar <itsparser@gmail.com>
2021-09-12 10:34:47 +0530

    :tada: Added the New Project as Explosives

---
+++ pkg/constant/default.go
@@ -0,0 +1,13 @@
+package constant
+
+// ASCIILogo is a sweet logo that is optionally printed to the command line window
+const ASCIILogo = `
+                                                                  
+###### #    # #####  #       ####   ####  # #    # ######  ####  
+#       #  #  #    # #      #    # #      # #    # #      #      
+#####    ##   #    # #      #    #  ####  # #    # #####   ####  
+#        ##   #####  #      #    #      # # #    # #           # 
+#       #  #  #      #      #    # #    # #  #  #  #      #    # 
+###### #    # #      ######  ####   ####  #   ##   ######  ####                         
+
+`
